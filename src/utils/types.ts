82074fe95812533760f13063ba969840dffb7afd
Aakash Jha <hey.a2v10@gmail.com>
2024-07-09 11:47:43 +0530

    updated types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,48 +1,52 @@
 export interface User {
-    email: string;
-    displayName: string;
-    photoURL: string;
-    createdAt: string;
-    updatedAt: string;
-  }
-  
-  export interface Group {
-    name: string;
-    description: string;
-    createdBy: string;
-    admin: string;
-    members: string[];
-    createdAt: string;
-    updatedAt: string;
-  }
-  
-  export interface Expense {
-    title: string;
-    amount: number;
-    category: string;
-    createdBy: string;
-    createdAt: string;
-    updatedAt: string;
-    splits: Split[];
-  }
-  
-  export interface Split {
-    userId: string;
-    amount: number;
-    paid: boolean;
-  }
-  
-  export interface Notification {
-    title: string;
-    message: string;
-    read: boolean;
-    createdAt: string;
-  }
-  
-  
-  export interface Balance {
-    userId: string;
-    balance: number;
-    updatedAt: string;
-  }
-  
+  id: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Group {
+  id: string;
+  name: string;
+  description: string;
+  createdBy: string;
+  admin: string;
+  members: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Expense {
+  id: string;
+  title: string;
+  amount: number;
+  category: string;
+  createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+  splits: Split[];
+}
+
+export interface Split {
+  id: string;
+  userId: string;
+  amount: number;
+  paid: boolean;
+}
+
+export interface Notification {
+  id: string;
+  title: string;
+  message: string;
+  read: boolean;
+  createdAt: string;
+}
+
+export interface Balance {
+  id: string;
+  userId: string;
+  balance: number;
+  updatedAt: string;
+}
