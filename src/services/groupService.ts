c5c58e73f4a0b36d195c42cd107a986a06018a3f
Aakash Jha <hey.a2v10@gmail.com>
2024-07-10 11:41:32 +0530

    added get group by id

--- src/services/groupService.ts
+++ src/services/groupService.ts
@@ -1,1 +1,16 @@
 // write your firebase services here!
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+import { Group } from '../utils/types';
+
+export const getGroupById = async (groupId: string): Promise<Group | null> => {
+  const groupDocRef = doc(db, 'groups', groupId);
+  const groupDocSnap = await getDoc(groupDocRef);
+
+  if (groupDocSnap.exists()) {
+    return { id: groupDocSnap.id, ...groupDocSnap.data() } as Group;
+  } else {
+    console.log('No such document!');
+    return null;
+  }
+};
