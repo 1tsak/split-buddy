621069267a9b90a3efbc05b71b5a3565b9519eb3
Aakash Jha <hey.a2v10@gmail.com>
2024-07-10 18:15:00 +0530

    formatted code

--- src/components/UseDialog.tsx
+++ src/components/UseDialog.tsx
@@ -1,25 +1,28 @@
-import { useState } from 'react';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-
-import Dialog from './NotificationDialog';
+import { useState } from "react";
+import Dialog from "./NotificationDialog";
+import { IoIosNotificationsOutline } from "react-icons/io";
 
 const NotificationBell = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const openDialog = ()=>{
-        setIsOpen((prev) => !prev);
-    }
+  const [isOpen, setIsOpen] = useState(false);
+  const openDialog = () => {
+    setIsOpen((prev) => !prev);
+  };
 
   return (
     <>
       <header className="App-header">
-        <button
-          onClick={openDialog}
-          className="px-1 py-1 text-black rounded"
-        >
-          <NotificationsIcon />
+        <button onClick={openDialog} className="px-1 py-1 text-black rounded">
+          <IoIosNotificationsOutline
+            color="#121212"
+            className="cursor-pointer"
+            size={25}
+          />
         </button>
-        <Dialog isOpen={isOpen} title="Recent Activities" setIsOpen={setIsOpen}/>
-          
+        <Dialog
+          isOpen={isOpen}
+          title="Recent Activities"
+          setIsOpen={setIsOpen}
+        />
       </header>
     </>
   );
