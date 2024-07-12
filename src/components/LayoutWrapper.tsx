cff6e01ff15302c170e9318d17f4b332a5465145
Akarshit Batra <akarshit.batra@geekyants.com>
2024-07-12 15:17:27 +0530

    fixed the sidebar

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -13,7 +13,7 @@ const LayoutWrapper: React.FC = () => {
       <Navbar />
 
       {!hideSidebar ? (
-        <main className="flex w-full ">
+        <main className="flex flex-auto ">
           <Sidebar />
           <div className="flex-grow">
             <Outlet />
