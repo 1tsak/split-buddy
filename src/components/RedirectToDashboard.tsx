20462a822f076d5e05d67bf46832053c95b229c8
Prince Vasoya <princevasoya@Princes-MacBook-Pro.local>
2024-07-12 14:56:05 +0530

    redirect to dashboard if login

--- src/components/RedirectToDashboard.tsx
+++ src/components/RedirectToDashboard.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebaseConfig';
+import { CircularProgress, Box } from '@mui/material';
 
 const RedirectToDashboard: React.FC<{ children: JSX.Element }> = ({ children }) => {
   const [user, loading] = useAuthState(auth);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
+  if (loading) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
   return user ? <Navigate to="/dashboard" /> : children;
 };
 
