1a974e01b90c41a2451225b0913ee8de3a8247b8
Abhishek Krishnan Rathaur <abhishekkrrathaur2004@gmail.com>
2024-07-16 14:44:50 +0530

    Little changes in notification page

--- src/components/NotificationPage.tsx
+++ src/components/NotificationPage.tsx
@@ -35,8 +35,8 @@ const NotificationPage = () => {
         {notifications && notifications.length > 0 ? (
           notifications.map((notification,index) => (
             <div key={index} className='bg-white shadow-md rounded-lg p-4'>
-              <h3 className='font-bold text-lg mb-2'>{notification.title}</h3>
-              <p className='text-gray-700 mb-2'>{notification.message}</p>
+              <h3 className=' text-lg mb-2'>{notification.title}</h3>
+              <p className='text-gray-700 font-bold mb-2'>{notification.message}</p>
               <span className='text-gray-500 text-sm'>
                 {moment(notification.createdAt).fromNow()}
               </span>
