5f09605cb5af76f42c82eac7dda5d5ee29f360b9
Abhishek Krishnan Rathaur <abhishekkrrathaur2004@gmail.com>
2024-07-16 11:40:29 +0530

    Fixed to 2 decimal place

--- src/components/BillCreation.tsx
+++ src/components/BillCreation.tsx
@@ -76,7 +76,8 @@ const BillCreation = () => {
     })
     const splits: Split[] = groupMember.map((user, index) => {
       let amount = formData.splits[index]?.checked
-        ? ( splitsMember > 0 ?  remainingAmount / splitsMember : 0)
+        ? ( splitsMember > 0 ?        parseFloat((remainingAmount / (splitsMember || 1)).toFixed(2))
+        : 0)
         : 0;// console.log(amount);
         if(customBill[index]){
           amount = customBill[index];
@@ -171,8 +172,9 @@ const BillCreation = () => {
     });
     const remainingSplits = splits.filter((split, idx) => split.checked && customBill[idx] === undefined);
     remainingSplits.forEach((split) => {
-      split.amount = remainingAmount / (remainingSplits.length || 1);
+      split.amount = parseFloat((remainingAmount / (remainingSplits.length || 1)).toFixed(2));
     });
+       
     setFormData((prev) => ({ ...prev, splits }));
     splitEqually();
   };
