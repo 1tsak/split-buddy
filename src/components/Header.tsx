3f06fecc506dc00d827ed1d4f0789a50fac48a6e
Prince Vasoya <princevasoya@Princes-MacBook-Pro.local>
2024-07-17 21:40:46 +0530

    updated / page if logged in show user profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,48 @@
 import { Link } from "react-router-dom";
-import HeroSection from "./HeroSection"
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import HeroSection from "./HeroSection";
+
 const Header = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [userPhoto,setUserPhoto]= useState("");
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setAuthenticated(!!user);
+      const photoUrl=user?.photoURL;
+      setUserPhoto(photoUrl);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <>
-      <div className="px-10 h-screen flex  flex-col">
-        <div className="h-[10%] flex justify-between items-center">
-            <div className="w-40"> 
-            <Link  to={"/"}>
-            <span className="font-semibold text-main ">
-          <img src="logo_main.png" alt="site logo" />
-        </span>
+    <div className="px-10 h-screen flex flex-col">
+      <div className="h-[10%] flex justify-between items-center">
+        <div className="w-40">
+          <Link to="/">
+            <img src="logo_main.png" alt="site logo" className="font-semibold text-main" />
+          </Link>
+        </div>
+        {authenticated ? (
+          <Link to='/dashboard'>
+          <img src={userPhoto} alt="userPhoto" className="h-10 rounded-full"/></Link>
+        ) : (
+          <div className="flex gap-5 items-center">
+            <Link to="/signup">Sign up</Link>
+            <Link className="px-3 py-2 text-white bg-main rounded-md" to="/login">
+              Login
             </Link>
-            </div>
-            <div className="flex gap-5 items-center">
-            <Link className="" to={"/signup"}>Sign up</Link>
-            <Link className="px-3 py-2 text-white bg-main rounded-md" to={"/login"}>Login</Link>
-            </div>
-        </div>
-        <div className="h-[90%] px-2 md:px-10 flex flex-col justify-center md:flex-row md:justify-evenly md:gap-4 ">
-            <HeroSection/>
-        </div>
+          </div>
+        )}
       </div>
-    </>
-  )
+      <div className="h-[90%] px-2 md:px-10 flex flex-col justify-center md:flex-row md:justify-evenly md:gap-4">
+        <HeroSection />
+      </div>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
