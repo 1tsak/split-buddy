8496b96ddf3231d5d6ccd56959f65f47369fc8d9
Akarshit Batra <akarshit.batra@geekyants.com>
2024-07-17 12:10:14 +0530

    grouped the transaction data with date and is no dashboard

--- src/components/TransactionContainer.tsx
+++ src/components/TransactionContainer.tsx
@@ -1,14 +1,15 @@
 import React, { FC, useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebaseConfig";
-import { TransactionCardType } from "../utils/types";
+import { TransactionCardType, TransactionGroupType } from "../utils/types";
 import { getUserTransactoins } from "../services/dashboardServices";
 import TransactionCard from "./TransactionCard";
 import { Box, CircularProgress } from "@mui/material";
+import TransactionDateContainer from "./TransactionDateContainer";
 
 const TransactionContainer: FC = () => {
   const [user, loading] = useAuthState(auth);
-  const [tData, setTData] = useState<TransactionCardType[]>();
+  const [tData, setTData] = useState<TransactionGroupType>();
   const fetchData = async () => {
     const data = await getUserTransactoins(user?.uid as string);
     setTData(data)
@@ -17,7 +18,7 @@ const TransactionContainer: FC = () => {
     fetchData();
   }, []);
 
-  if(tData && tData.length==0){
+  if(tData && Object.keys(tData).length==0){
     return <div className="rounded-xl border-main border-[1px] p-2 flex-auto">
     <p className="text-2xl font-extrabold">Recent Transactions</p>
     <p className="text-gray-400 text-sm"></p>
@@ -32,8 +33,8 @@ const TransactionContainer: FC = () => {
       <p className="text-2xl font-extrabold">Recent Transactions</p>
       <p className="text-gray-400 text-sm"></p>
       <div className="overflow-scroll min-h-[80vh] max-h-[80vh]">
-        {tData ? tData.map((trns,i)=>{
-            return <TransactionCard key={i} data={trns}/>
+        {tData ? Object.keys(tData).map((trns,i)=>{
+            return <TransactionDateContainer key={i} date={trns} transactions={tData[trns]}/>
         }) :(
             <Box
               sx={{
