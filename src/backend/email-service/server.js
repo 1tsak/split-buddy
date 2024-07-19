117b9d0f4de5e68c0a0c8fbc807b2c8db3e52003
Prince Vasoya <princevasoya@Princes-MacBook-Pro.local>
2024-07-19 12:12:31 +0530

    added dotenv

--- src/backend/email-service/server.js
+++ src/backend/email-service/server.js
@@ -1,14 +1,13 @@
-
-
-
 const express = require('express');
 const sgMail = require('@sendgrid/mail');
 const cors = require('cors');
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-sgMail.setApiKey("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");
+// Set SendGrid API key from environment variable
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -20,7 +19,6 @@ app.post('/send-welcome-email', async (req, res) => {
     to: email,
     from: 'vasoyaprince14@gmail.com',
     subject: 'Welcome to Split Buddy!',
-    test:"hello from split buddy apxp",
     html: `
       <p>Hello ${displayName || "User"},</p>
       <p>Welcome to Split Buddy! You have successfully signed up.</p>
@@ -29,8 +27,8 @@ app.post('/send-welcome-email', async (req, res) => {
   };
 
   try {
-   const mail= await sgMail.send(msg);
-   console.log(mail)
+    const mail = await sgMail.send(msg);
+    console.log(mail);
     res.status(200).send('Welcome email sent');
   } catch (error) {
     console.error('Error sending welcome email:', error);
