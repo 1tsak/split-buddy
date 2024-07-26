77b85106c264bc73efe15b7156a05c9a90955e80
Aakash Jha <hey.a2v10@gmail.com>
2024-07-26 12:08:41 +0530

    wip error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,19 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { StyledEngineProvider } from "@mui/material";
+import "./i18next.ts";
+import "rsuite/dist/rsuite.min.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import { StyledEngineProvider } from '@mui/material';
-import './i18next.ts'; 
-import 'rsuite/dist/rsuite.min.css'; 
-import { Provider } from 'react-redux';
-import { store } from './store/store.ts';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <StyledEngineProvider injectFirst>
       <Provider store={store}>
-      <App />
+        <App />
       </Provider>
-      </StyledEngineProvider>
-    </BrowserRouter>
+    </StyledEngineProvider>
+  </BrowserRouter>
 );
