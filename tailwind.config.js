a8cd9e97832226dd312e0a07271fbe4390885594
Akarshit Batra <akarshit.batra@geekyants.com>
2024-08-02 15:26:40 +0530

    changed the color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       colors:{
-        "main":"#687EEF",
+        "main":"#7EEF",
         "secondary":"#9CF5BA",
         "tertiary":"#d968ef",
         "card-color":"#F0F4FF"
